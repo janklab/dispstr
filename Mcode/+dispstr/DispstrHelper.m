9ce20ad5a28367eeea0e857e7c800df68a77bcc5
Andrew Janke <floss@apjanke.net>
2019-01-13 23:38:49 -0500

    Fix prettyprinting for N-D arrays

--- Mcode/+dispstr/DispstrHelper.m
+++ Mcode/+dispstr/DispstrHelper.m
@@ -2,18 +2,9 @@ classdef DispstrHelper
     
     methods (Static)
         function disparray(x)
-        if ~ismatrix(x)
-            fprintf('%s %s\n', size2str(size(x)), class(x));
-            return
-        end
         strs = dispstrs(x);
-        lens = strlen(strs);
-        widths = max(lens);
-        formats = sprintfv('%%%ds', widths);
-        format = strjoin(formats, '   ');
-        for i = 1:size(x, 1)
-            fprintf([format '\n'], strs{i,:});
-        end
+        out = dispstr.internal.prettyprint_array(strs);
+        disp(out);
         end
     end
 end
