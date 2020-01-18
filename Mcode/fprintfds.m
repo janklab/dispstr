7090603506ebacc6f948b7a7ac1a1a7932c8cff3
Andrew Janke <andrew@apjanke.net>
2020-01-18 13:58:00 -0500

    Add sprintfds and fprintfds

---
+++ Mcode/fprintfds.m
@@ -0,0 +1,36 @@
+function fprintfds(varargin)
+%FPRINTFDS A fprintf that respects dispstr()
+%
+% out = fprintfds(fmt, varargin)
+% out = fprintfds(fid, fmt, varargin)
+%
+% This is a variant of fprintf that respects the dispstr() function.
+% See the documentation for SPRINTFDS for details on how it works.
+% 
+% Examples:
+%
+% fprintfds('The value is: %s', myUserDefinedObject)
+%
+% See also:
+% SPRINTFDS
+
+args = varargin;
+if isnumeric(args{1})
+  fid = args{1};
+  args(1) = [];
+else
+  fid = [];
+end
+
+fmt = args{1};
+args(1) = [];
+
+str = sprintfds(fmt, args{:});
+
+if isempty(fid)
+  fprintf('%s', str);
+else
+  fprintf(fid, '%s', str);
+end
+
+end
