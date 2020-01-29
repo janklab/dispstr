9721cb99e96f24c838acb8b9869049e9f581d6fb
Andrew Janke <andrew@apjanke.net>
2020-01-29 15:58:06 -0500

    Add errords and warningds

--- Mcode/sprintfds.m
+++ Mcode/sprintfds.m
@@ -17,20 +17,7 @@ function out = sprintfds(fmt, varargin)
 % See also:
 % FPRINTFDS
 
-args = varargin;
-for i = 1:numel(args)
-  arg = args{i};
-  if isobject(arg)
-    if isstring(arg) && isscalar(arg)
-      % NOP; we want to keep the single string and let it be interpolated
-      % as normal
-    elseif isa(arg, 'datetime') || isa(arg, 'duration') || isa(arg, 'calendarDuration')
-      % NOP; these already support %s conversions
-    else
-      args{i} = dispstr(arg);
-    end
-  end
-end
+args = dispstrlib.internal.convertArgsForPrintf(varargin);
 
 out = sprintf(fmt, args{:});
 
